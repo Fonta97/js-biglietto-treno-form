820e484297ef8805abd9d9d32cce03ebb855fb6c
Fonta97 <fontana.alessandro97@gmail.com>
2023-11-20 15:46:15 +0100

    strutturazione js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,29 @@
+
+
 // richiesta input ad utente su lunghezza viaggio e età
-const wthIsAKilometer = parseInt (document.getElementById('distance').value);
-const userName = (const document.getElementById('name').value;);
+const wthIsAKilometer = document.getElementById("km");
+const userName = document.getElementById("km");
+const sendButton = document.querySelector("send");
 
-console.log(wthIsAKilometer, userName);
 
+console.log(wthIsAKilometer, userName, sendButton);
 
-// dichiarazione variabili costanti prezzo per km e scontistiche in base all'età
-let priceKm = wthIsAKilometer * 0.21;
-let finalPrice
-// calcolo sconto se presente
-if (userAge < 18) {
-    finalPrice = wthIsAKilometer * 0.8
-} else if (userAge > 65) {
-    finalPrice = wthIsAKilometer * 0.6
-}
+sendButton.addEventListener("click",
+    function () {
+        let priceKm = wthIsAKilometer * 0.21;
+        let finalPrice
+        // calcolo sconto se presente
+        if (userAge === "minorenne") {
+            finalPrice = wthIsAKilometer * 0.8
+        } else if (userAge === "over65") {
+            finalPrice = wthIsAKilometer * 0.6
+        }
 
-console.log(priceKm, finalPrice)
+        console.log(priceKm, finalPrice)
 
-finalPrice = (priceKm).toFixed(2)
-
+        finalPrice = (priceKm).toFixed(2)
+    }
+)
 console.log(finalPrice)
 
 document.getElementById("ticket_price").innerHTML = "Paga o il trenino Thomas ti verrà a prendere di notte:" + finalPrice + "€";
