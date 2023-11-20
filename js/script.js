c2f1302a336571c37b22d83f3a5fcdfaa9f3648b
Fonta97 <fontana.alessandro97@gmail.com>
2023-11-20 16:40:17 +0100

    js completo (sono INELUTTABILE)

--- js/script.js
+++ js/script.js
@@ -2,28 +2,35 @@
 
 // richiesta input ad utente su lunghezza viaggio e età
 const wthIsAKilometer = document.getElementById("km");
-const userName = document.getElementById("km");
-const sendButton = document.querySelector("send");
+const userName = document.getElementById("nome");
+const sendButton = document.getElementById("send");
+const userAge = document.getElementById("eta");
 
 
 console.log(wthIsAKilometer, userName, sendButton);
 
 sendButton.addEventListener("click",
     function () {
-        let priceKm = wthIsAKilometer * 0.21;
+        kmPercorsi = wthIsAKilometer.value;
+        etasconto = userAge.value;
+        let priceKm = kmPercorsi * 0.21;
         let finalPrice
-        // calcolo sconto se presente
-        if (userAge === "minorenne") {
-            finalPrice = wthIsAKilometer * 0.8
-        } else if (userAge === "over65") {
-            finalPrice = wthIsAKilometer * 0.6
-        }
+        nome = userName.value
 
+        // calcolo sconto se presente
+        if (etasconto == "minorenne") {
+            finalPrice = priceKm * 0.8
+            document.getElementById("ticket_price").innerHTML = nome + ", ecco il tuo prezzo scontato da minorenne: " + finalPrice + " €";
+        } else if (etasconto == "over65") {
+            finalPrice = priceKm * 0.6
+            document.getElementById("ticket_price").innerHTML = nome + ", ecco il tuo prezzo scontato over65: " + finalPrice + " €";
+        } else {
+            finalPrice = priceKm
+            document.getElementById("ticket_price").innerHTML = nome + ", ecco il tuo prezzo: " + finalPrice + " €";
+        }
         console.log(priceKm, finalPrice)
 
-        finalPrice = (priceKm).toFixed(2)
+
+        console.log(finalPrice)
     }
 )
-console.log(finalPrice)
-
-document.getElementById("ticket_price").innerHTML = "Paga o il trenino Thomas ti verrà a prendere di notte:" + finalPrice + "€";
